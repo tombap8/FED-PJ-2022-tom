e2a9a072ef429784c569caddb090ec9ef9ecad46
Tom B. Parker <tombap8@hotmail.com>
2023-02-10 11:51:43 +0900

    ss

--- 002.CSS학습/05.3DAni/js/cube.js
+++ 002.CSS학습/05.3DAni/js/cube.js
@@ -12,7 +12,55 @@
 *************************************/
 
 //////////////////// 로드구역 ///////////////////
-window.addEventListener("DOMContentLoaded",()=>{
+window.addEventListener("DOMContentLoaded", () => {
     console.log("로딩완료!");
+
+    // 1. 대상선정
+    // 1-1.이벤트대상 : .btngo
+    const btngo = document.querySelector(".btngo");
+
+    // 1-2.변경대상 : .cube
+    const cube = document.querySelector(".cube");
+
+    // console.log(btngo,cube);
+
+    // 2. 버튼 클릭 이벤트설정
+    btngo.onclick = () => {
+        // 1. 큐브에 클래스 "on"넣기/빼기
+        cube.classList.toggle("on");
+        // toggle() 메서드는 지정한 클래스가
+        // 있으면 빼고 없으면 넣는다!
+
+        // 2. 현재 큐브에 "on"클래스가 있는 여부검사
+        let isOn = cube.classList.contains("on");
+        console.log("on있냐?", isOn);
+
+        // 3. 버튼 텍스트 변경하기
+        btngo.innerText = isOn ? "멈춰!" : "돌아!";
+        // 삼항연산자(조건연산자) -> 비?집:놀이동산
+    }; //////// click 이벤트 함수 /////
+
+    /********************************************** 
+    [ JS 클래스 컨트롤 내장객체 ]
+    classList 객체
+
+    -> 요소에 클래스를 넣거나 빼는 등 필요작업을
+    하는 객체
+
+    ((관련메서드))
+    1. add(클래스명) : 클래스추가
+    2. remove(클래스명) : 클래스제거
+    3. toggle(클래스명) : 클래스추가/제거
+    4. contains(클래스명) : 클래스있으면 true
+    5. replace(이전클래스명,변경클래스명)
+        : 특정클래스를 다른 클래스로 변경
+
+    -> 클래스 추가/제거시 콤마로 구분하여
+    여러개의 클래스를 추가하거나 제거할 수 있다!
+    예) 요소.classList.add("tt","cc","dd")
+    예) 요소.classList.remove("bb","ee")
+
+
+**********************************************/
 }); ////////////// 로드구역 ///////////////////
 //////////////////////////////////////////////
