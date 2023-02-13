231dbf53567c80f4636edbd13302a456853dee0a
Tom B. Parker <tombap8@hotmail.com>
2023-02-13 11:10:36 +0900

    Update main.js

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -15,9 +15,30 @@ window.addEventListener("DOMContentLoaded",()=>{
         "on"을 모두 지워서 선택된 li만
         일어서있는 CSS가 적용되게 한다!
     ************************************/
-   // 대상 : .mlist ul>li -> 이벤트+변경대상 일치
+   // 1. 대상선정 : .mlist ul>li 
+   // -> 이벤트+변경대상 일치
     const mlist = qsa(".mlist ul>li");
-    console.log(mlist);
+    // console.log(mlist);
+
+    // 2. 클릭이벤트 함수 설정하기
+    // for of사용! -> 배열/컬렉션 일때 사용!
+    for(let x of mlist){ // x 각 li요소
+        x.onclick = ()=>{
+
+            // 1. li에 클래스 초기화!
+            mlist.forEach(ele=>ele.classList.remove("on"));
+            // forEach((요소,순번,객체)=>{코드})
+
+            // 2.클릭된 li에 클래스 "on"넣기
+            x.classList.add("on");
+            // classList 객체사용!
+            // add()메서드 사용!
+
+        }; //////// click함수 //////
+
+    } ///////// for of /////////////
+
+
 
 
 }); ///////////// 로드구역 ////////////////
