4ccc92be3f85e87fa416820efbdbe1a95250b5ec
Tom B. Parker <tombap8@hotmail.com>
2023-02-20 13:08:04 +0900

    ss

---
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/intro.js
@@ -0,0 +1,11 @@
+// 인트로 페이지 JS - intro.js
+
+// 로딩호출 /////
+window.addEventListener("DOMContentLoaded",loadFn);
+
+// 로딩실행함수 /////
+function loadFn(){
+
+    console.log("로딩완료!");
+
+} /////////// loadFn 함수 ///////////////
