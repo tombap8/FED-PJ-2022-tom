b3a4a17d72e54854bd86a111b363a504dfc2687a
Tom B. Parker <tombap8@hotmail.com>
2023-02-28 15:05:29 +0900

    ss

--- 001.HTML학습/05.Nav_PJ/nav06/js/common.js
+++ 001.HTML학습/05.Nav_PJ/nav06/js/common.js
@@ -9,4 +9,56 @@ window.addEventListener("DOMContentLoaded",loadFn);
 ******************************************/
 function loadFn(){
 
+    console.log("로딩완료!");
+
+    /************************************ 
+        GNB메뉴 객체 데이터를 이용한
+        HTML태그 만들어 넣기!
+    ************************************/
+   // 대상: .gnb
+   const gnb = document.querySelector(".gnb");
+//    console.log(gnb);
+
+    // html코드 담을 변수
+    let hcode = "";
+
+
+    hcode += `<ul>`;
+    // 1. 상위메뉴 반복코드 생성
+    // mdata객체를 가져와서 반복시킴 -> for in문!
+    // console.log(mdata);
+
+    for(let tm in mdata){ // tm은 mdata의 속성명
+
+        hcode+=
+            `
+            <li>
+                <a href="#">${tm}</a>
+                <div class="smenu">
+                    <h2>
+                        ${tm}
+                    </h2>
+                    <div class="swrap">
+                        <dl>
+                            <dt>오노마</dt>
+                            <dd>에센스</dd>
+                            <dd>스킨/토너</dd>
+                            <dd>크림</dd>
+                            <dd>선크림</dd>
+                            <dd>세트</dd>
+                        </dl>
+    
+                    </div>
+                </div>
+            </li>
+    
+        `;
+    } ///////// for in문 //////////////////////
+
+    hcode += "</ul>";
+
+    
+
+
+
 } ///////////// loadFn 함수 ////////////////
