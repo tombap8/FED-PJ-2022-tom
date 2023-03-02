05c87a200371c605d043f5865e3671c3bab37d0d
Tom B. Parker <tombap8@hotmail.com>
2023-03-02 13:09:33 +0900

    Update category.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
@@ -21,6 +21,16 @@ function loadFn(){
 
     console.log("로딩완료!");
 
-    // 1. 변경 대상 선정
+    // 1. 변경 대상 선정 ////////////////////////
+    // (1) 서브타이틀
+    const stit = document.querySelector(".stit");
+    // (2) 서브메뉴
+    const lnb = document.querySelector(".lnb");
+    // (3) 컨텐츠 상위박스(카테고리 클래스 넣기)
+    const cont = document.querySelector(".cont");
+    // (4) title요소(타이틀 내용에 카테고리명 앞에추가)
+    const titag = document.querySelector("title");
+
+    console.log(stit,lnb,cont,titag);
 
 } ////////// loadFn함수 //////////////
