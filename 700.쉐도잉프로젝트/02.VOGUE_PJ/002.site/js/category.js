308f7aedaaead7056a9a520c3d9bbdc216440722
Tom B. Parker <tombap8@hotmail.com>
2023-03-02 12:28:53 +0900

    ss

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/category.js
@@ -0,0 +1,11 @@
+// 보그 PJ 카테고리 페이지 JS - category.js
+
+//////// 로딩구역 ///////////////////
+window.addEventListener("DOMContentLoaded",loadFn);
+
+//// 로드함수 ////////////////////////
+function loadFn(){
+
+    console.log("로딩완료!");
+
+} ////////// loadFn함수 //////////////
---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -0,0 +1,11 @@
+// 보그 PJ 링크시스템 JS - linksys.js
+
+//////// 로딩구역 ///////////////////
+window.addEventListener("DOMContentLoaded",linkFn);
+
+//// 링크시스템 로드함수 ////////////////////////
+function linkFn(){
+
+    console.log("링크 로딩완료!");
+
+} ////////// linkFn함수 //////////////
