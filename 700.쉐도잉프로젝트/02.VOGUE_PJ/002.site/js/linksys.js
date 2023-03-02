39a7baa43b8d21007aa6617aa6b1f6062fd8098d
Tom B. Parker <tombap8@hotmail.com>
2023-03-02 12:54:00 +0900

    Update linksys.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/linksys.js
@@ -8,4 +8,44 @@ function linkFn(){
 
     console.log("링크 로딩완료!");
 
+    // 1. 링크 대상 선정 : 
+    // (1) GNB : .gnb a
+    const gnb = document.querySelectorAll(".gnb a");
+    // console.log(gnb);
+    // (2) 로고 : .logo a
+    const logo = document.querySelector(".logo a");
+
+    // 2. 클릭이벤트 설정하기
+    // (1) GNB 클릭설정 //////////
+    for(let x of gnb){
+        x.onclick = (e) => {
+            // 클릭이동기능막기
+            e.preventDefault();
+
+            // (1) 클릭된 a요소 텍스트 읽기
+            let atxt = x.innerText.toLowerCase().trim();
+            // toLowerCase() -> 소문자변환
+            // 참고) toUpperCase() -> 대문자변환
+            // trim() -> 앞뒤공백제거
+
+            console.log(atxt);
+
+            // (2) 서브 페이지 이동하기
+            location.href = "category.html?cat="
+            +encodeURIComponent(atxt);
+
+        }; ///////// click ///////
+
+    } /////////// for  of ///////////////
+
+    // (2) 로고 클릭설정 //////////////
+    logo.onclick = (e) => {
+        e.preventDefault();
+
+        // 홈으로 이동하기
+        location.href = "index.html";
+
+    }; ////////// click ///////////
+
+
 } ////////// linkFn함수 //////////////
