01be7369f03d7366b27c493cd2a1e0961928bb74
Tom B. Parker <tombap8@hotmail.com>
2023-03-03 15:47:55 +0900

    Update main.js

--- 004.JS학습/Racing_PJ/js/main.js
+++ 004.JS학습/Racing_PJ/js/main.js
@@ -171,6 +171,14 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
         // (4) 메시지 박스 보이기
         msg.style.display = "block"; 
+        msg.style.zIndex = "101"; 
+
+        // (5) 전체 반투명 암전주기
+        document.body.innerHTML += 
+        "<div style='position:fixed;top:0;left:0;width:100vw;height:100vh;background-color:#000;opacity:0.5;z-index:100'></div>";
+
+        // (6) 버튼 위로 올리기
+        btns.style.zIndex = "200";
 
 
 
