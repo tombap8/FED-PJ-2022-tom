5761e3bb9881b643b04fabb14f1a85082fc1ee4f
Tom B. Parker <tombap8@hotmail.com>
2023-03-07 16:06:49 +0900

    Create ban.js

---
+++ 001.HTML학습/05.Nav_PJ/nav06/js/ban.js
@@ -0,0 +1,9 @@
+// 네비유형 6 : 배너셋팅 JS - ban.js //////
+window.addEventListener("DOMContentLoaded",setBan);
+
+// 배너셋팅함수 //////////////
+function setBan(){
+    // 1. 호출확인
+    console.log("배너야~!");
+
+} /////////////// setBan 함수 ////////////
