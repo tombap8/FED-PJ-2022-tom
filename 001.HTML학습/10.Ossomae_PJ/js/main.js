bad935fa0b4ffae8f6a41e88ad65861499f2080e
Tom B. Parker <tombap8@hotmail.com>
2023-03-14 10:21:23 +0900

    Update main.js

--- 001.HTML학습/10.Ossomae_PJ/js/main.js
+++ 001.HTML학습/10.Ossomae_PJ/js/main.js
@@ -5,6 +5,36 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     console.log("로딩완료!");
 
+    // 대상: .gbx
+    const gbx = document.querySelector(".gbx");
+
+    document.querySelector(".rb")
+    .onclick = () => {
+
+        console.log("오른쪽!");
+        // 이동대상: .gbx>div
+        let tg = gbx.querySelectorAll("div");
+
+        // 첫번째 자식요소 div 맨뒤로이동
+        // appendChild(첫번째요소)
+        gbx.appendChild(tg[0]);
+
+    }; ////////// click /////////////
+
+    // 왼쪽버튼
+    document.querySelector(".lb")
+    .onclick = () => {
+
+        console.log("왼쪽!");
+        // 이동대상: .gbx>div
+        let tg = gbx.querySelectorAll("div");
+
+        // 마지막 자식요소 div 맨앞로이동
+        // insertBefore(마지막요소,첫번째요소)
+        gbx.insertBefore(tg[tg.length-1],tg[0]);
+
+    }; ////////// click /////////////
+
     
 }); ///////////// 로딩구역 //////////////////////
 /////////////////////////////////////////////////
