b0ee4397a7295dd182c068d154a1302c57fbfd34
Tom B. Parker <tombap8@hotmail.com>
2023-03-15 14:55:14 +0900

    Update main3.js

--- 001.HTML학습/09.ShoppingMall_PJ/js/main3.js
+++ 001.HTML학습/09.ShoppingMall_PJ/js/main3.js
@@ -26,4 +26,64 @@ window.addEventListener("DOMContentLoaded", loadFn);
     함수명: loadFn
     기능: 로딩후 이벤트설정 및 슬라이드 기능
 ******************************************/
+function loadFn() {
+    console.log("로딩완료!");
+
+    // 슬라이드번호 변수
+    let snum = 0;
+
+    // 1. 대상선정
+    // 1-1. 이벤트 대상: .abtn
+    const abtn = document.querySelectorAll(".abtn");
+    // console.log(abtn);
+
+    // 1-2. 변경 대상: #slide > li
+    const slide = document.querySelectorAll("#slide>li");
+    // 슬라이드 개수
+    let scnt = slide.length;
+    console.log("슬개수:",scnt);
+    
+    // 1-3. 블릿 대상: .indic li
+    const indic = document.querySelectorAll(".indic li");
+
+    // 2. 슬라이드 변경함수 만들기
+    const goSlide = (seq) => {
+        console.log("슬고우!",seq);
+
+        // 1. 방향에 따른 분기
+        // 1-1. 오른쪽버튼 클릭시: seq===1일때
+        if(seq){
+            // 슬라이드번호 증가!
+            snum++;
+            console.log("오른!",snum);
+
+        }
+        // 1-2. 왼쪽버튼 클릭시: seq===0일때
+        else{
+            // 슬라이드번호 감소!
+            snum--;
+            console.log("왼쪽!",snum);
+        }
+
+        // 2. 한계값 체크 : 
+        // 처음이전->끝
+        if(snum===-1) snum = scnt-1;
+        //끝다음->처음
+        else if(snum === scnt) snum = 0;
+
+        // 3. 이동하기 : 해당순번 슬라이드 li에 클래스"on"넣기
+        // 변경대상: slide변수(#slide li)
+        // 전체초기화!
+        slide.forEach(ele=>ele.classList.remove("on"));
+        // 해당순번li에 클래스넣기
+        slide[snum].classList.add("on");
+    }; ////////// goSlide함수 ///////////
+
+    // 3. 대상에 이벤트 설정하기
+    abtn.forEach((ele, idx) => {
+        ele.onclick = () => {
+            goSlide(idx);
+        }; ///// click함수 //////
+    }); /////// forEach //////////
+}
  /////////////// loadFn 함수 //////////////
