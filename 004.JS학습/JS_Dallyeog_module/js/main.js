f8dec81ab381f09ccd14305a7e39697ad5fd03c2
Tom B. Parker <tombap8@hotmail.com>
2023-03-27 13:19:35 +0900

    Update main.js

--- 004.JS학습/JS_Dallyeog_module/js/main.js
+++ 004.JS학습/JS_Dallyeog_module/js/main.js
@@ -25,3 +25,12 @@ calbx2.initDallyeok();
 
 
 /// 제이쿼리 라이브러리를 html페이지 상단에 호출후 사용!
+$(".myipt").click(function(){
+    console.log(this);
+    // 해당박스 달력보이기
+    $(this).next().find(".calender").show();
+}); /////// click /////////////
+
+$(".calender").click(function(){
+    $(this).hide();
+});
