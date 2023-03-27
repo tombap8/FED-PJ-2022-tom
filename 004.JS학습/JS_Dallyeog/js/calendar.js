9f9235d3a0a889e485b71a04aba2d1dc8243df36
Tom B. Parker <tombap8@hotmail.com>
2023-03-27 11:24:44 +0900

    ss

--- 004.JS학습/JS_Dallyeog/js/calendar.js
+++ 004.JS학습/JS_Dallyeog/js/calendar.js
@@ -61,9 +61,10 @@ function MakeDallyeok() {
             for (let i = 0; i < thisFirst.getDay(); i++) {
                 // cg(i);
                 // 반복횟수 만큼 배열 앞쪽에 추가한다!
+                // 전달은 클래스 "bm"으로 구분함!
                 // 전달 마지막 날짜부터! -> prevLast.getDate()
                 dset.unshift(`
-                <span style="color:#ccc" class="bdt">
+                <span style="color:#ccc" class="bm">
                     ${prevLast.getDate() - i}
                 </span>`);
                 // 마지막날짜 - i증가변수 = 1씩작아지는 숫자추가됨
@@ -79,10 +80,11 @@ function MakeDallyeok() {
         } /////////////// for ///////////////////
 
         // 3. 다음달 나머지 칸 삽입하기
+        // 다음달은 클래스 "am"으로 구분함!
         // 반복구성: 1부터 2주분량정도 넣는다!
         for (let i = 1; i < 14; i++) {
             dset.push(`
-            <span style="color:#ccc" class="bdt">${i}</span>`);
+            <span style="color:#ccc" class="am">${i}</span>`);
         } /////////// for /////////////////
 
         // cg(dset);
@@ -120,6 +122,17 @@ function MakeDallyeok() {
                     let cmonth = monthTit.innerText;
                     // 일
                     let cdate = ele.innerText;
+
+                    // 전달/다음달은 span태그가 있으므로 구분함!
+                    let isSpan = ele.querySelector("span");
+                    cg(isSpan);
+                    // 없을 경우 null값이 나옴 -> if문에서 false처리됨!
+                    if(isSpan){ // null이 아닐때만 true처리되어 들어감!
+                        // 
+                        let cls = isSpan.classList.contains("bm");
+                        cg(cls);
+
+                    } /////////// if //////////////
 
                     // 최종날짜 데이터
                     let comp = cyear + "-" + addZero(cmonth) + "-" + addZero(cdate);
