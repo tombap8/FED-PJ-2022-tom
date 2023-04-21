ffefcdce4d23d6111d257126aa9d0902d8dccbd3
Tom B. Parker <tombap8@hotmail.com>
2023-04-21 10:46:42 +0900

    Update main.js

--- 004.JS학습/Parallax_PJ/js/main.js
+++ 004.JS학습/Parallax_PJ/js/main.js
@@ -36,8 +36,34 @@ function loadFn(){
     // 7. 스크롤 이벤트함수 만들기
     window.addEventListener("scroll",()=>{
 
+        let elpos = retVal(tg1[2]);
         // 테스트 - 3번째 텍스트 위치값
-        console.log(retVal(tg1[2]));
+        console.log(elpos);
+
+        // 패럴렉스 범위 : 윈도우높이값 ~ 0까지!
+        // 화면에서 완전히 사라질때 범위는 0이 아니고
+        // 요소의 -높이값만큼 이다!
+        if(elpos < winH && elpos > 0){
+
+            // 1. 위치이동값 계산
+            let x = elpos * setH2 / winH;
+            console.log("x:",-x);
+
+            // 2. 해당요소에 위치이동 CSS반영
+            tg1[2].style.cssText = `
+                transform:translateY(${-x}px);
+            `;
+            // cssText속성은 style속성값을 직접넣어준다!
+
+            // 대상요소의 트랜스폼 Y축이동
+            // 위치이동의 계산원리
+            // -> 윈도우화면 : 위치값 = 정한범위 : 실제이동값
+            // -> 윈도우가 1000px : 500px = 200px : x?
+            // 실제이동값 = 위치값*정한범위 / 윈도우화면
+            // x = elpos * setH2 / winH
+
+
+        } /////// if : 패럴렉스 범위 ///////
 
     }); /////////// scroll /////////////////
 
