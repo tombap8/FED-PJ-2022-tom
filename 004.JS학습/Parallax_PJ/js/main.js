fd4e2917c04a735a13eb072704d04617f2e7a605
Tom B. Parker <tombap8@hotmail.com>
2023-04-21 10:06:22 +0900

    ss

---
+++ 004.JS학습/Parallax_PJ/js/main.js
@@ -0,0 +1,16 @@
+// 패럴렉스 PJ JS - main.js
+
+// 로드 이벤트 설정
+window.addEventListener("DOMContentLoaded",loadFn);
+
+// 로드함수 ////
+function loadFn(){
+
+    console.log("로딩완료!");
+
+    // 부드러운 스크롤 적용하기!
+    startSS();
+
+
+
+} ////////////// loadFn ///////////////////////////
