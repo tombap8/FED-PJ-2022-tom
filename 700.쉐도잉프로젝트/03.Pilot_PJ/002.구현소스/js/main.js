8610c2a9da2f419845e21f71873cfac44a247875
Tom B. Parker <tombap8@hotmail.com>
2023-05-01 12:21:59 +0900

    ss

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/main.js
@@ -21,79 +21,14 @@ import autoScroll from "./jquery-autoScroll.js";
 import menuFn from "./mainjs/menu.js";
 // 배너기능함수 가져오기
 import banFn from "./mainjs/ban.js";
+// 공통 데이터 가져오기
+import comData from "./tempData/data-common.js";
 
 
 //###### 상단영역 메뉴 뷰 템플릿 셋팅하기 #######
 // Vue.component(내가지은요소명,{옵션})
 Vue.component("top-comp",{
-    template:`
-                    
-    <header class="top ibx">
-    <!-- 1-1.로고 -->
-    <h1 id="logo">
-        <a href="#">
-            <img src="images/main_logo.png" alt="파일럿로고" />
-        </a>
-    </h1>
-    <!-- 1-2.GNB메뉴 : 3개(배너,하단제외) -->
-    <nav class="gnb">
-        <ul>
-            <li class="bld">배너순번 li 숨기기</li>
-            <li>
-                <a href="#men">MEN</a>
-            </li>
-            <li>
-                <a href="#women">WOMEN</a>
-            </li>
-            <li>
-                <a href="#style">STYLE</a>
-            </li>
-        </ul>
-    </nav>
-
-    <!-- 햄버거버튼 -->
-    <div class="ham">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-
-    <!-- 전체메뉴박스 -->
-    <!-- 같은 부모박스 아래 있어야 z-index비교로 햄버거버튼
-이 위에 올 수 있다! -->
-    <div class="mbox">
-        <!-- 
-        배경 동영상 : muted 소리없음, loop 반복재생
-    -->
-        <video src="images/disc2018.mp4" class="bgm" loop muted></video>
-        <!-- 전체메뉴 -->
-        <nav class="mlist">
-            <dl>
-                <dt><a href="#">MEN</a></dt>
-                <dd><a href="#">T-SHIRT</a></dd>
-                <dd><a href="#">JACKET</a></dd>
-                <dd><a href="#">TRAINING WARE</a></dd>
-                <dd><a href="#">BEACH WARE</a></dd>
-            </dl>
-            <dl>
-                <dt><a href="#">WOMEN</a></dt>
-                <dd><a href="#">T-SHIRT</a></dd>
-                <dd><a href="#">JACKET</a></dd>
-                <dd><a href="#">TRAINING WARE</a></dd>
-                <dd><a href="#">BEACH WARE</a></dd>
-            </dl>
-            <dl>
-                <dt><a href="#">STYLE</a></dt>
-                <dd><a href="#">COLLECTION</a></dd>
-                <dd><a href="#">SEASON AD</a></dd>
-                <dd><a href="#">STAR &amp; NEWS</a></dd>
-                <dd><a href="#">MAIN ITEM</a></dd>
-            </dl>
-        </nav>
-    </div>
-</header>
-
-    `,
+    template:comData.tarea,
 }); ////////////// 상단영역 Vue component /////////////
 
 //###### 상단영역 뷰 인스턴스 생성하기 ##########
---
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/tempData/data-common.js
@@ -0,0 +1,77 @@
+// 공통 data 객체 셋팅 JS - data-common.js
+
+const comData = {
+    // 상단영역 html data
+    tarea: `                    
+    <header class="top ibx">
+        <!-- 1-1.로고 -->
+        <h1 id="logo">
+            <a href="#">
+                <img src="images/main_logo.png" alt="파일럿로고" />
+            </a>
+        </h1>
+        <!-- 1-2.GNB메뉴 : 3개(배너,하단제외) -->
+        <nav class="gnb">
+            <ul>
+                <li class="bld">배너순번 li 숨기기</li>
+                <li>
+                    <a href="#men">MEN</a>
+                </li>
+                <li>
+                    <a href="#women">WOMEN</a>
+                </li>
+                <li>
+                    <a href="#style">STYLE</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- 햄버거버튼 -->
+        <div class="ham">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <!-- 전체메뉴박스 -->
+        <!-- 같은 부모박스 아래 있어야 z-index비교로 햄버거버튼
+    이 위에 올 수 있다! -->
+        <div class="mbox">
+            <!-- 
+            배경 동영상 : muted 소리없음, loop 반복재생
+        -->
+            <video src="images/disc2018.mp4" class="bgm" loop muted></video>
+            <!-- 전체메뉴 -->
+            <nav class="mlist">
+                <dl>
+                    <dt><a href="#">MEN</a></dt>
+                    <dd><a href="#">T-SHIRT</a></dd>
+                    <dd><a href="#">JACKET</a></dd>
+                    <dd><a href="#">TRAINING WARE</a></dd>
+                    <dd><a href="#">BEACH WARE</a></dd>
+                </dl>
+                <dl>
+                    <dt><a href="#">WOMEN</a></dt>
+                    <dd><a href="#">T-SHIRT</a></dd>
+                    <dd><a href="#">JACKET</a></dd>
+                    <dd><a href="#">TRAINING WARE</a></dd>
+                    <dd><a href="#">BEACH WARE</a></dd>
+                </dl>
+                <dl>
+                    <dt><a href="#">STYLE</a></dt>
+                    <dd><a href="#">COLLECTION</a></dd>
+                    <dd><a href="#">SEASON AD</a></dd>
+                    <dd><a href="#">STAR &amp; NEWS</a></dd>
+                    <dd><a href="#">MAIN ITEM</a></dd>
+                </dl>
+            </nav>
+        </div>
+    </header>
+
+    `,
+    barea: "",
+}; ///////////////// comData //////////////
+
+
+// 내보내기
+export default comData;
