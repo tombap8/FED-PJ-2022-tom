7ab96ec2b218d50a51ac1eb2a05d42bdaf81a41c
Tom B. Parker <tombap8@hotmail.com>
2023-05-03 13:17:50 +0900

    Update sub.js

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -112,59 +112,101 @@ function sinsangFn() {
 
         // 재귀호출하기(비동기호출-setTimeout)
         // 조건: call_sts 상태값이 1일때만 호출함!
-        if(call_sts) setTimeout(moveList, 40);
+        if (call_sts) setTimeout(moveList, 40);
     } ////////// moveList 함수 //////////////
 
     // 신상품 이동함수 최초호출
-    moveList();
+    // moveList();
 
     // 신상품 리스트에 마우스 오버시 멈춤
     // 신상품 리스트에 마우스 아웃시 이동
     // hover(함수1,함수2)
     flist.hover(
-      function(){ // over
-        call_sts = 0; // 콜백중단!
-      },
-      function(){ // out
-        call_sts = 1; // 콜백허용!
-        moveList(); // 함수재호출!
-      }); ////////// hover //////////////
+        function () {
+            // over
+            call_sts = 0; // 콜백중단!
+        },
+        function () {
+            // out
+            call_sts = 1; // 콜백허용!
+            moveList(); // 함수재호출!
+        }
+    ); ////////// hover //////////////
 
-
-      /********************************************** 
+    /********************************************** 
         신상품 리스트 li에 마우스 오버시 정보보이기
         1. 대상 : .flist li
         2. 정보구분법 : li의 클래스명으로 신상품정보와
                     매칭하여 상품정보박스를 동적으로
                     생성하여 애니메이션을 주어 보이게함
       **********************************************/
-     flist.find("li").hover(
-      function(){ // over
-        // 1. 클래스 정보 알아내기
-        let clsnm = $(this).attr("class");
+    flist.find("li").hover(
+        function () {
+            // over
+            // 1. 클래스 정보 알아내기
+            let clsnm = $(this).attr("class");
 
-        // 2. 클래스 이름으로 셋팅된 신상정보 객체 데이터 가져오기
-        let gd_info = sinsang[clsnm];
+            // 2. 클래스 이름으로 셋팅된 신상정보 객체 데이터 가져오기
+            let gd_info = sinsang[clsnm];
 
-        console.log(clsnm,gd_info);
+            // console.log(clsnm,gd_info);
 
-        // 3. 상품정보박스 만들고 보이게하기
-        // 마우스 오버된 li자신 (this)에 넣어준다!
-        $(this).append(`<div class="ibox"></div>`);
-        // .ibox에 상품정보 넣기
-        // ^는 특수문자이므로 정규식에 넣을때 역슬래쉬와 함께씀
-        // -> /\^/
-        $(".ibox").html(gd_info.replace(/\^/g,"<br>"))
-        .animate({
-          top:"110%",
-          opacity: 1
-        },300,"easeOutCirc");
-      },
-      function(){ // out
-        // ibox 나갈때 지우기
-        $(".ibox").remove();
-      }); //////////// hover ///////////////////////
-     
+            // 3. 상품정보박스 만들고 보이게하기
+            // 마우스 오버된 li자신 (this)에 넣어준다!
+            $(this).append(`<div class="ibox"></div>`);
+            // .ibox에 상품정보 넣기
+            // ^는 특수문자이므로 정규식에 넣을때 역슬래쉬와 함께씀
+            // -> /\^/
+            $(".ibox").html(gd_info.replace(/\^/g, "<br>")).animate(
+                {
+                    top: "110%",
+                    opacity: 1,
+                },
+                300,
+                "easeOutCirc"
+            );
+        },
+        function () {
+            // out
+            // ibox 나갈때 지우기
+            $(".ibox").remove();
+        }
+    ); //////////// hover ///////////////////////
 
+    /*********************************************** 
+        스크롤 위치가 신상품 박스가 보일때만 움직이기
+      ***********************************************/
+    // JS의 getBoundingClientRect()  의 값과 같은것은?
+    // 적용박스 offset().top위치값 - scroll바 위치값
 
+    // 1. 대상요소위치값
+    let tgpos = flist.offset().top;
+
+    // 2. 스크롤위치변수
+    let scTop = 0;
+
+    // 3. 화면높이값
+    let winH = $(window).height();
+
+    // 4. 스크롤 이벤트함수 ///////////
+    $(window).scroll(function () {
+        // 1. 스크롤위치값
+        scTop = $(this).scrollTop();
+
+        // 2. gBCR 값 구하기
+        let gBCR = tgpos - scTop;
+
+        console.log("gBCR:", gBCR);
+
+        // 3. 신상품 리스트 이동/멈춤 분기하기
+        // (1) 이동기준 gBCR값이 화면높이보다 작고 0보다 클때 이동
+        if (gBCR < winH && gBCR > 0) {
+            call_sts = 1; // 콜백허용!
+            moveList(); // 함수재호출!
+        } ////// if ///////////
+        // (2) 기타경우 멈춤
+        else {
+            call_sts = 0; // 콜백중단!
+        } ///// else ///////////////
+    }); ////////// scroll /////////////
 } ///////////// sinsangFn 함수 ////////////////
