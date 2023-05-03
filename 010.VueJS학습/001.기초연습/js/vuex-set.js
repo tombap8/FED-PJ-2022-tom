0e49e2f24e701ed27af49cb71b46c9eee7aec855
Tom B. Parker <tombap8@hotmail.com>
2023-05-03 16:33:26 +0900

    Update vuex-set.js

--- 010.VueJS학습/001.기초연습/js/vuex-set.js
+++ 010.VueJS학습/001.기초연습/js/vuex-set.js
@@ -1,2 +1,32 @@
 // 뷰엑스 스토어 구현 JS 
 
+// 1. 상단영역 컴포넌트 셋팅
+Vue.component("top-area",{
+    template:``,
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+});
+// 2. 메인영역 컴포넌트 셋팅
+Vue.component("main-area",{
+    template:``,
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+});
+// 3. 하단영역 컴포넌트 셋팅
+Vue.component("info-area",{
+    template:``,
+    data(){
+        return{}
+    },
+    methods:{
+
+    }
+});
