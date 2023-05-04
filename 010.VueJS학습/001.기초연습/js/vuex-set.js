416d4fb8315781e41ca9f22ec0f21be464032ef6
Tom B. Parker <tombap8@hotmail.com>
2023-05-04 13:03:23 +0900

    ss

--- 010.VueJS학습/001.기초연습/js/vuex-set.js
+++ 010.VueJS학습/001.기초연습/js/vuex-set.js
@@ -107,5 +107,28 @@ new Vue({
             txt:"도시소개에 오신것을 환영합니다!"
         });
         // store.commit('initSet',"https://img.freepik.com/premium-vector/city-illustration_23-2147514701.jpg");
-    }
+    }, /////////// created /////
+    // 제이쿼리는 DOM에 직접 작용하므로 mounted에 구현함!
+    mounted(){
+        // 링크 클릭시 a에 클래스 on주기
+        $(".gnb a").click(function(){
+            $(this).addClass("on")
+            .parent().siblings()
+            .find("a").removeClass("on");
+
+            // 박스 애니
+            showBx();
+        }); ////////// click ///////////
+
+        function showBx(){
+            // 이미지와 설명박스 순서대로 나타나기
+            $("main img")
+            .css({opacity:0})
+            .delay(500).fadeTo(500,1);
+
+            $("main p")
+            .css({opacity:0})
+            .delay(1000).fadeTo(500,1);
+        } ////// showBx ////
+    } ////////// mounted //////
 }); //////// Vue 인스턴스 /////////
