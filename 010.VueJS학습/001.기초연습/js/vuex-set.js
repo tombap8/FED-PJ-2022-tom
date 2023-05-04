36636626cde2d23d361950f267d7201304053053
Tom B. Parker <tombap8@hotmail.com>
2023-05-04 10:21:43 +0900

    ss

--- 010.VueJS학습/001.기초연습/js/vuex-set.js
+++ 010.VueJS학습/001.기초연습/js/vuex-set.js
@@ -1,8 +1,23 @@
 // 뷰엑스 스토어 구현 JS 
 
+// [1] 컴포넌트 셋팅하기 //////
 // 1. 상단영역 컴포넌트 셋팅
 Vue.component("top-area",{
-    template:``,
+    template:`
+        <header>
+            <ul class="gnb">
+                <li>
+                    <a href="#">서울</a>
+                </li>
+                <li>
+                    <a href="#">부산</a>
+                </li>
+                <li>
+                    <a href="#">제주</a>
+                </li>
+            </ul>
+        </header>
+    `,
     data(){
         return{}
     },
@@ -12,7 +27,14 @@ Vue.component("top-area",{
 });
 // 2. 메인영역 컴포넌트 셋팅
 Vue.component("main-area",{
-    template:``,
+    template:`
+        <main>
+            <img src="https://www.shutterstock.com/image-photo/songpagu-seoul-south-korea-september-260nw-2094838786.jpg" alt="지역이미지">
+            <p>
+                대한민국의 수도인 서울을 지방자치단체인 특별시로 부르는 명칭이다. 한반도 중앙에 있으며, 한강을 사이에 두고 남북으로 펼쳐져 있다. 북쪽 끝은 도봉구 도봉동, 동쪽 끝은 강동구 상일동, 남쪽 끝은 서초구 원지동, 서쪽 끝은 강서구 오곡동이다. 시청은 중구 을지로1가(태평로1가 31)에 있다.
+            </p>
+        </main>
+    `,
     data(){
         return{}
     },
@@ -22,7 +44,13 @@ Vue.component("main-area",{
 });
 // 3. 하단영역 컴포넌트 셋팅
 Vue.component("info-area",{
-    template:``,
+    template:`
+        <footer>
+            <address>
+                서울시 강남구 역삼동 119 뷰엑스B
+            </address>
+        </footer>
+    `,
     data(){
         return{}
     },
@@ -30,3 +58,9 @@ Vue.component("info-area",{
 
     }
 });
+
+// [2] 뷰 인스턴스 생성하기 //////
+// 대상요소:  #app
+new Vue({
+    el:"#app",
+}); //////// Vue 인스턴스 /////////
