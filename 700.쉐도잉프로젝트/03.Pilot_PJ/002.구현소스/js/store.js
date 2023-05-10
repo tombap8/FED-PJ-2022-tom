a0b2c380a1e8178727032302d2fd6092613fa0c9
Tom B. Parker <tombap8@hotmail.com>
2023-05-10 11:39:55 +0900

    Update store.js

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/store.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/store.js
@@ -1,35 +1,42 @@
 // 서브 페이지 뷰엑스 스토어 셋팅 JS  - store.js
 const store = new Vuex.Store({
-    state:{
+    state: {
         // 서브데이터 셋업
         subData: {
-            "남성":{
+            남성: {
+                // 남성메뉴
+                menu: ["NEW ARRIVAL", "WINDBREAKER", "BEACH STYLE", "SPORT STYLE"],
                 // 배너개수
-                cnt:3,
+                cnt: 3,
                 // 카테고리명
-                cat:"men",
+                cat: "men",
             },
-            "여성":{
+            여성: {
+                // 여성메뉴
+                menu: ["NEW ARRIVAL", "SPORTY FASHION", "FREE STYLE", "COMFORTABLE STYLE"],
                 // 배너개수
-                cnt:3,
+                cnt: 3,
                 // 카테고리명
-                cat:"women",
+                cat: "women",
             },
-            "스타일":{
+            스타일: {
+                // 스타일메뉴
+                menu: ["NEW ARRIVAL", "GOLF LIFE", "CAMPING STYLE", "SPORT STYLE"],
                 // 배너개수
-                cnt:5,
+                cnt: 5,
                 // 카테고리명
-                cat:"style",
+                cat: "style",
             },
         },
+        // 공통처리 메뉴 변수
+        menu: ["NEW ARRIVAL", "WINDBREAKER", "BEACH STYLE", "SPORT STYLE"],
         // 공통처리 배너개수 변수
         cnt: 3,
         // 공통처리 카테고리명 변수
-        cat:"men",
-
+        cat: "men",
     },
     // state 데이터 변경 메서드구역!
-    mutations:{
-
-    }
-})
+    mutations: {
+        // 
+    },
+});
