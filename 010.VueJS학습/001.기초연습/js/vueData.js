120f98920dd880c53e005830b501aa33acd557ed
Tom B. Parker <tombap8@hotmail.com>
2023-05-12 13:21:20 +0900

    Update vueData.js

--- 010.VueJS학습/001.기초연습/js/vueData.js
+++ 010.VueJS학습/001.기초연습/js/vueData.js
@@ -12,9 +12,11 @@ import store from "./vuedata-store.js";
 // 엑시오스로 가져온 데이터는 {data:{실제데이터}} 형식으로
 // 한번더 랩핑하여 들어온다! 따라서 이 데이터만 사용하려면
 // $store.state.items.data -> data속성까지 써줘야 바로사용됨!
+
 // 반면 뷰엑스 스토어의 actions에서 받은 제이슨 데이터는
 // 원본 그대로 할당하여 들어오므로
 // $store.state.items -> 원본 변수를 바로 사용해야함!!!
+
 Vue.component("my-comp",{
     template:`
         <div class="grid">
