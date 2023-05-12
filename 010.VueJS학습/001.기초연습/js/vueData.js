b1c5dd8ee2d0ad39d3347b6d25b7785dd548616c
Tom B. Parker <tombap8@hotmail.com>
2023-05-12 13:20:31 +0900

    ss

--- 010.VueJS학습/001.기초연습/js/vueData.js
+++ 010.VueJS학습/001.기초연습/js/vueData.js
@@ -4,15 +4,25 @@
 import store from "./vuedata-store.js";
 
 
-// 컴포넌트 생성하기
+// [ 컴포넌트 생성하기 ]
+// 이미지 src속성 바인드시
+// (v.idx > 50 ? 1 : v.idx) 문장해석:
+// 데이터 idx 값이 50초과이면 1을 넣고 아니면 idx데이터 넣기
+
+// 엑시오스로 가져온 데이터는 {data:{실제데이터}} 형식으로
+// 한번더 랩핑하여 들어온다! 따라서 이 데이터만 사용하려면
+// $store.state.items.data -> data속성까지 써줘야 바로사용됨!
+// 반면 뷰엑스 스토어의 actions에서 받은 제이슨 데이터는
+// 원본 그대로 할당하여 들어오므로
+// $store.state.items -> 원본 변수를 바로 사용해야함!!!
 Vue.component("my-comp",{
     template:`
         <div class="grid">
-            <div v-for="(v,i) in $store.state.items.data">
+            <div v-for="(v,i) in $store.state.items">
                 <img 
                 v-bind:src="
                     './img_gallery/' +
-                    v.idx +
+                    (v.idx > 50 ? 1 : v.idx) +
                     '.jpg'
                 " 
                 alt="dress" />
@@ -41,12 +51,18 @@ new Vue({
     },
     // 뷰인스턴스 생성직후(가상돔/돔 생성전)
     created(){
+
+        // 뷰엑스 스토어 액션스구역 메서드 initData호출하기!
+        store.dispatch('initData');
+
+
         // 엑시오스 사용하여 제이슨 데이터 가져오기
         // axios.get(제이슨파일).then(데이터=>담을변수=데이터)
-        axios.get("./js/data.json")
-        .then(x=>store.state.items = x);
-        // 스토어의 items 전역변수에 셋팅함!
-        console.log(store.state.items);
+
+        // axios.get("./js/goods.json")
+        // .then(x=>store.state.items = x);
+        // // 스토어의 items 전역변수에 셋팅함!
+        // console.log(store.state.items);
 
     }, //////// created /////////////
 })
--- 010.VueJS학습/001.기초연습/js/vuedata-store.js
+++ 010.VueJS학습/001.기초연습/js/vuedata-store.js
@@ -1,4 +1,27 @@
 // 뷰JS 데이터처리 뷰엑스 스토어 JS - vuedata-store.js
+
+// 제이슨 데이터 처리위해 제이슨 불러오기!
+/* 
+    import jsnData from "./goods.json";
+    일반 JS호출과 같은 방식으로 제이슨 파일을 호출하면
+    아래와 같은 MIME 전송형식 type에러가 발생함!
+    ____________________________________________________
+    "Failed to load module script: Expected a JavaScript
+     module script but the server responded with a MIME type
+      of "application/json". Strict MIME type checking is 
+      enforced for module scripts per HTML spec."
+      ____________________________________________
+
+      ((해결방법!))
+      import 변수 from 제이슨파일경로 assert{type:"json"}
+
+      assert{type:"json"} -> import시 형식을 지정해주는 객체!
+      이것으로 제이슨 형식을 맞춰서 전송타입 오류를 없앤다!!!
+*/
+import jsnData from "./goods.json" assert{type:"json"};
+
+
+
 const store = new Vuex.Store({
     // (1) 데이터 셋팅구역 :
     state:{
@@ -9,11 +32,33 @@ const store = new Vuex.Store({
     // (2) 데이터 변경 메서드 구역 : 
     // 호출시 commit()사용!
     mutations:{
+
+        // 제이슨 데이터 속성변수 업데이트 하기
+        setData(st,pm){ // st - state변수, pm - 전달변수
+            // state구역의 items변수에 제이슨 데이터 담기!
+            st.items = pm;
+            console.log("뮤테:",pm);
+        }, ///////// setData /////////////////
         
     },
     // (3) 백엔관련 코딩 비동기처리 메서드 구역 : 
     // 호출시 dispatch()사용
     actions:{
+        // 제이슨 데이터 로드하기 메서드
+        initData({commit}){ 
+            // 액션스 메서드 전달값으로 {commit}을 쓰면
+            //  뮤테이션 구역으로 바로 commit사용가능!
+            // 제이슨 데이터 변수에 할당!
+            const result = jsnData;
+            console.log("액션:",result);
+
+            // state의 items변수변경 메서드호출
+            // 뮤테이션스의 메서드 호출은? 
+            // -> commit('메서드',전달값)
+            commit('setData',result);
+
+
+        }, ///////// initData /////////
         
     }
 }); /////////// 뷰엑스 인스턴스 ////////
