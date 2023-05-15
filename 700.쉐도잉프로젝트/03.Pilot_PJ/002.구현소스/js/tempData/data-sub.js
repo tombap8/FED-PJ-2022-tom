678794b5846ceb96a6e35dbf95ddac68c439df16
Tom B. Parker <tombap8@hotmail.com>
2023-05-15 11:18:24 +0900

    Update data-sub.js

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/tempData/data-sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/tempData/data-sub.js
@@ -72,7 +72,9 @@ const subData = {
         :class="
             'cont c2 '+ $store.state.cat
         " id="c2">
-            <h2 class="c2tit js-reveal">2023 BEACH SPECIAL WINDBREAKER</h2>
+            <h2 class="c2tit js-reveal">
+                2023 {{$store.state.menu[1]}}
+            </h2>
         </section>
     `,
     // 컨텐츠영역3 : 일반소개1
