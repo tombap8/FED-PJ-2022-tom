469178f29c22d060d22323a15c412d1863b5aca3
Tom B. Parker <tombap8@hotmail.com>
2023-05-17 11:31:03 +0900

    Update glist-router.js

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-router.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-router.js
@@ -4,7 +4,35 @@
 
 // 라우터 템플릿 만들기
 let Glist = {
-    template: `<div class="trip router">필터</div>`,
+    template: `
+        <section>
+            <!-- 필터옵션 체크박스구역 -->
+            <div id="optbx">
+                <label for="men">남성</label>
+                <input type="checkbox" id="men">
+                <label for="women">여성</label>
+                <input type="checkbox" id="women">
+                <label for="style">스타일</label>
+                <input type="checkbox" id="style">
+            </div>
+
+            <!-- 상품리스트박스 -->
+            <div class="grid">
+                <div v-for="(v,i) in $store.state.gdata">
+                    <img 
+                        v-bind:src="
+                        './images/goods/'+
+                        v.cat +
+                        '/'+v.ginfo[0]+'.png'  
+                        " alt="dress" />
+                    <aside>
+                        <h2>{{v.ginfo[1]}}</h2>
+                        <h3>{{v.ginfo[3]}}</h3>
+                    </aside>
+                </div>
+            </div>
+        </section>
+    `,
 };
 let Paging = {
     template: `<div class="trip router">페이징</div>`,
