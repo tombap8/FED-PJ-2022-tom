183ade807e7a4fc0fb7fd18674c43a9db2a72f30
Tom B. Parker <tombap8@hotmail.com>
2023-05-22 11:30:14 +0900

    Update main.js

--- 005.jQuery학습/003.응용구현/Megabox_PJ/js/main.js
+++ 005.jQuery학습/003.응용구현/Megabox_PJ/js/main.js
@@ -1,5 +1,6 @@
 // 메가박스 PJ 메인 JS - main.js
-$(function () { //// jQB1 //////////////////
+$(function () {
+    //// jQB1 //////////////////
 
     // 메뉴 a요소 기본이동 막기!
     $(".gnb a,.indic a").click(function (e) {
@@ -10,7 +11,6 @@ $(function () { //// jQB1 //////////////////
     // 이벤트 대상: .gnb li + .indic li
     // 변경 대상: html,body
     $(".gnb li,.indic li").click(function (e) {
-
         ////// 광스크롤막기 /////////////
         if (psts) return; //돌아가!
         psts = 1; //불허용상태변경!
@@ -22,7 +22,6 @@ $(function () { //// jQB1 //////////////////
         // 0. 클릭된 li순번 구해오기
         let idx = $(this).index();
         //// console.log("순번:" + idx);
-
 
         // 1. 하위a요소의 href값 읽어오기
         // let idnm = $("a", this).attr("href");
@@ -49,27 +48,24 @@ $(function () { //// jQB1 //////////////////
         // 참고: 가로스크롤 이동속성: scrollLeft
         // 스크롤 이동대상: html,body
         // -> 범용브라우저에서 사용하는 스크롤대상
-        $("html,body").animate({
-            scrollTop: pos + "px"
-        }, 1200, "easeOutQuint",pageAction); //// animate /////
-
+        $("html,body").animate(
+            {
+                scrollTop: pos + "px",
+            },
+            1200,
+            "easeOutQuint",
+            pageAction
+        ); //// animate /////
 
         // 4. 클릭된 li요소에 class="on" 넣기
-        $(".gnb li").eq(idx).addClass("on")
-            .siblings().removeClass("on");
-        $(".indic li").eq(idx).addClass("on")
-            .siblings().removeClass("on");
+        $(".gnb li").eq(idx).addClass("on").siblings().removeClass("on");
+        $(".indic li").eq(idx).addClass("on").siblings().removeClass("on");
         //다른 형제 li들 class="on" 지움
 
         // 6. li순번과 pno와 일치하기! /////////////////////
         pno = idx;
         //// console.log("페이지번호:" + pno);
-
-
     }); ///////////// click ///////////////
-
-
-
 }); ///////////// jQB1 ///////////////////
 
 /////////////////////////////////////////////////
@@ -82,8 +78,8 @@ let autoOK;
 // 동영상
 let mv;
 
-
-$(function () { //// jQB2 //////////////////////////
+$(function () {
+    //// jQB2 //////////////////////////
 
     // 변경대상: .gbx
     let gbx = $(".gbx");
@@ -103,7 +99,6 @@ $(function () { //// jQB2 //////////////////////////
 
     /// 오른쪽 이동버튼 클릭시 ////////
     $(".rb").click(function () {
-
         // 광클금지 //////
         if (prot) return;
         prot = 1;
@@ -121,12 +116,10 @@ $(function () { //// jQB2 //////////////////////////
         if (!autoOK) mvPlay();
 
         // console.log("지우기호출여부:" + autoOK);
-
     }); ///////////// click /////////////
 
     /// 왼쪽 이동버튼 클릭시 /////////
     $(".lb").click(function () {
-
         // 광클금지 //////
         if (prot) return;
         prot = 1;
@@ -146,24 +139,19 @@ $(function () { //// jQB2 //////////////////////////
         // 중앙에 있는 포스터의 동영상으로 play함!
         if (!autoOK) mvPlay();
 
-
         // console.log("지우기호출여부:" + autoOK);
-
     }); ///////////// click ////////////////
 
     //// 이동버튼 클릭시 (하단일경우) 동영상 재생하기 ////
     let mvPlay = () => {
         // 1. 가운데 오는 동영상 정보읽기
-        let center =
-            "mv/" + gbx.find("img").eq(2).attr("data-mv");
+        let center = "mv/" + gbx.find("img").eq(2).attr("data-mv");
         // // console.log(center);
         // 2. 동영상 src 변경하기
         mv.attr("src", center);
         // 3. 동영상 재생하기
         mv.get(0).play();
     }; ////////////// mvPlay 함수 /////////////////////
-
-
 
     // 인터발용변수
     let autoI;
@@ -189,7 +177,6 @@ $(function () { //// jQB2 //////////////////////////
         autoT = setTimeout(autoSlide, 5000);
     }; //////////// clearAuto 함수 ///////////
 
-
     // 동영상요소
     mv = $("#mv");
 
@@ -197,7 +184,6 @@ $(function () { //// jQB2 //////////////////////////
     //////// 1. 영화포스터 클릭시 영화예고편 보여주기 /////
     // 대상: .gbx img
     $(".gbx img").click(function () {
-
         // 0. 되돌리기 버튼 보이기
         $(".rtn").fadeIn(300);
 
@@ -214,18 +200,16 @@ $(function () { //// jQB2 //////////////////////////
 
         // 순번이 1일 경우 왼쪽버튼 클릭 트리거!
         if (idx === 1) $(".lb").trigger("click");
-
         // 순번이 3일 경우 오른쪽버튼 클릭 트리거!
         else if (idx === 3) $(".rb").trigger("click");
         // 트리거 메서드: 선택자.trigger(이벤트명)
-
 
         // 1. 영화포스터 네비 작아지게 하단 이동 애니메이션
         $("#gbx").css({
             width: "40%",
             Transform: "translate(-50%, 130%)",
             minWidth: "500px",
-            transition: "all .6s ease-in-out"
+            transition: "all .6s ease-in-out",
         }); ////////////// css //////////////
 
         // 2. 포스터 자동넘기기 완전 지우기(다시실행안함!!!)
@@ -252,9 +236,7 @@ $(function () { //// jQB2 //////////////////////////
         // 동영상요소.get(0) -> 미디어컬렉션 get(0) 을 꼭사용!
         // 미디어 재생은 play() 메서드
         mv.get(0).play();
-        
     }); //////////////// click /////////////////////////
-
 
     ////////////////////////////////////////////////
     ////////// video태그 기능 컨트롤 ////////////////
@@ -267,26 +249,26 @@ $(function () { //// jQB2 //////////////////////////
     // 대상: .btnpp img
     // 제이쿼리 메서드 : hover(함수1,함수2)
     $(".btnpp img").hover(
-        function(){ // over - 진한이미지
+        function () {
+            // over - 진한이미지
             // 이미지 경로 읽어오기
             let csrc = $(this).attr("src");
             // 이미지 경로 변경하기 : ".png" -> "-1.png"
             // JS메서드 replace(바꿀값,바뀔값)
-            csrc = csrc.replace(".png","-1.png");
+            csrc = csrc.replace(".png", "-1.png");
             // console.log("현재경로:",csrc);
             // 실제 이미지 변경하기
-            $(this).attr("src",csrc);
-
+            $(this).attr("src", csrc);
         },
-        function(){ // out - 흐린이미지
+        function () {
+            // out - 흐린이미지
             // 이미지 경로 읽어오기
             let csrc = $(this).attr("src");
             // JS메서드 replace(바꿀값,바뀔값)
-            csrc = csrc.replace("-1.png",".png");
+            csrc = csrc.replace("-1.png", ".png");
             // console.log("현재경로:",csrc);
             // 실제 이미지 변경하기
-            $(this).attr("src",csrc);
-
+            $(this).attr("src", csrc);
         }
     ); //////// hover ///////////
 
@@ -294,73 +276,87 @@ $(function () { //// jQB2 //////////////////////////
     // 대상: .btnpp img
     // 원리: 재생상태이면 멈추고 멈춤상태이면 재생한다!
     // 핵심: 동영상의 멈춤상태를 알아낼 수 있다!
-    $(".btnpp img").click(function(){
+    $(".btnpp img").click(function () {
         // 동영상 멈춤상태 알아내기 - paused 속성으로 알아냄!
         // 결과: true - 멈춤, false - 재생중(멈춤아님)
         let paused_sts = mv.get(0).paused;
-        console.log("비디오가 멈췄니?",paused_sts);
+        console.log("비디오가 멈췄니?", paused_sts);
 
-
+        // 1. 멈춤아니면(false) 동영상 멈추기
+        if (!paused_sts) {
+            // false일때 들어오려면 NOT(!)연산자사용
+            // 비디오 멈추기는 pause() 메서드!
+            mv.get(0).pause();
+            // 멈춤상태일 경우 진한재생버튼으로 변경!
+            $(this).attr("src", "images/vbt2-1.png");
+        } ///////// if ///////////
+        else {
+            // 멈춤상태(treu)일경우
+            // 비디오 재생은 play() 메서드!
+            mv.get(0).play();
+            // 멈춤상태일 경우 진한멈춤버튼으로 변경!
+            $(this).attr("src", "images/vbt1-1.png");
+        } /////////// else ////////////////
     }); //////////// click //////////////
-
-
-    
-
 
     ///// 3. 영화페이지 : 스와이퍼 적용하기 //////
     // swiper변수를 전역변수로 만들고 페이지액션에서 사용!
-    swiper = new Swiper('.swiper-container', {
+    swiper = new Swiper(".swiper-container", {
         slidesPerView: 1, //한 영역에 보여줄 슬라이드수
         spaceBetween: 0, //슬라이드 사이간격
         loop: true, //무한이동
-        pagination: { // 하단 블릿표시
-            el: '.swiper-pagination',
+        pagination: {
+            // 하단 블릿표시
+            el: ".swiper-pagination",
             clickable: true, //블릿클릭이동
         },
-        navigation: { // 양쪽이동버튼
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+        navigation: {
+            // 양쪽이동버튼
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
         // 자동넘김을 미리셋팅하면 본 페이지 오기전부터
         // 이미 넘어가고 있다.... 그러므로 메서드를 사용해야함!
-        autoplay: { // 자동넘김
-            delay: 3000,//사이간격시간
+        autoplay: {
+            // 자동넘김
+            delay: 3000, //사이간격시간
             disableOnInteraction: false,
             //스와이프 상호작용후 다시자동넘김옵션
             //(false가 기본값-상호작용후 사이간격시간
             //있다가 다시 자동넘김작동함)
         },
     }); //////// swiper ///////////////////////////
-    
+
     // 처음에 스와이퍼 자동넘김 멈추기
     swiper.autoplay.stop();
 
-    
     /// 4. 특별관 페이지 - photobox 플러그인 적용하기 ///
     $("#gallery").photobox();
-
-
-
-
 }); ////////////// jQB2 ///////////////////////////
 //////////////////////////////////////////////////
-
 
 /*///////////////////////////////////////
 	함수명: changeTime
 	기능: 초를 보내면 시분초 변환해주는 함수
 */ ///////////////////////////////////////
-function changeTime(sec) { //sec 초단위값
+function changeTime(sec) {
+    //sec 초단위값
     "use strict"; //엄격한 문법적용
     var pad = function (x) {
-        return (x < 10) ? "0" + x : x;
+        return x < 10 ? "0" + x : x;
     };
     var res; //결과값
-    if (sec < 3600) { // 한시간이 넘지 않으면 분,초만 필요함
-        res = pad(parseInt(sec / 60 % 60)) + ":" + pad(sec % 60);
-    } else { // 한시간이 넘으면 시,분,초가 모두 필요함
-        res = pad(parseInt(sec / (60 * 60))) + ":" + pad(parseInt(sec / 60 % 60)) + ":" + pad(sec % 60);
-
+    if (sec < 3600) {
+        // 한시간이 넘지 않으면 분,초만 필요함
+        res = pad(parseInt((sec / 60) % 60)) + ":" + pad(sec % 60);
+    } else {
+        // 한시간이 넘으면 시,분,초가 모두 필요함
+        res =
+            pad(parseInt(sec / (60 * 60))) +
+            ":" +
+            pad(parseInt((sec / 60) % 60)) +
+            ":" +
+            pad(sec % 60);
     }
     return res;
 } ///////////////// changeTime함수 //////////////////////////////////////////////////////////
