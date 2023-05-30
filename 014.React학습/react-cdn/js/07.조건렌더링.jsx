686984367117d9a08850602f9a67fb00247db36d
Tom B. Parker <tombap8@hotmail.com>
2023-05-30 15:02:40 +0900

    Update 07.조건렌더링.jsx

--- 014.React학습/react-cdn/js/07.조건렌더링.jsx
+++ 014.React학습/react-cdn/js/07.조건렌더링.jsx
@@ -57,7 +57,7 @@ function Developer(props){ // 호출시 전달되는 속성 props
 // 이미지경로 배열
 const wisrc = [
     "https://cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HYAONH6EGJBKIU5CJWWF343MKE.jpg",
-    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201608/04/htm_2016080484837486184.jpg"
+    "https://img3.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/202208/24/BoiledMovie/20220824133926904mopw.png"
 ];
 
 // 컴포넌트 호출하기1!
@@ -75,3 +75,58 @@ isDev={false}
 isrc={wisrc[1]}
 ialt="주먹왕 마동석입니다!" />,
 document.querySelector("#root2"));
+
+/********************************************** 
+    2. if문이 아닌 조건 표현하기
+    -> 조건식 && JSX표현식
+    조건이 true일때만 && 뒤의 JSX표현식이 출력됨!
+**********************************************/
+
+// 2-1. 제목을 찍기위한 타이틀 컴포넌트
+function Title(props){ // 컴포넌트 호출시 속성으로 tit셋팅!
+    return <h1>👨‍🔧개발자👩‍🔧가 좋아하는 {props.tit}</h1>
+} /////////// Title 컴포넌트 /////////////
+
+// 음식리스트
+const foods = ["스파게티","짜파게티","냉면","짜장면","마라탕"];
+// const foods = [];
+
+// 2-2. 반복리스트를 위한 컴포넌트 /////
+function FoodList(props){// 음식명을 fname담아서 보내준다!
+    return <li>개발자는 {props.fname} 좋아해!</li>
+} //////////// FoodList 컴포넌트 ////////////////
+
+// 2-3. 개발자 선호 음식 리스트 출력 컴포넌트 ////
+function WishList(props){ // wlist속성에 담아 보내준다!
+    // 위시리스트 담기
+    const myfood = props.wlist;
+    // 코드리턴
+    return(
+        <React.Fragment>
+            <Title tit="음식" />
+            {/* 음식 위시리스트의 길이가 0보다 클때만 출력 */}
+            {
+                myfood.length > 0 &&
+                <div>
+                    <h2>
+                        개발자가 좋아하는 음식은 모두 
+                        {myfood.length}가지 입니다!
+                    </h2>
+                    <ul>
+                        {
+                            // 배열변수.map() 메서드사용!
+                            // map(변수=>{표현식})
+                            // 변수는 화살표함수 안으로 배열값전달
+                            myfood.map(x=> <FoodList fname={x} /> )
+                        }
+                    </ul>
+                </div>
+            }
+        </React.Fragment>
+    );
+} /////////// WishList 컴포넌트 //////////////////
+
+
+// 컴포넌트 출력하기
+ReactDOM.render(<WishList wlist={foods} />,
+document.querySelector("#root2"))
