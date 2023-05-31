3f964c223ceedda78d1af94905e90973688782a6
Tom B. Parker <tombap8@hotmail.com>
2023-05-31 12:20:18 +0900

    Update 07.조건렌더링.jsx

--- 014.React학습/react-cdn/js/07.조건렌더링.jsx
+++ 014.React학습/react-cdn/js/07.조건렌더링.jsx
@@ -249,13 +249,18 @@ function MakeWork(props){
 // 특이사항 : 변경버튼 클릭시 MakeWork 컴포넌트의 데이터를 
 //          변경하여 다시 출력하도록 한다!
 function ExpComp(props){ // isChg 는 true/false값 받는 속성
-    let result = props.isChg;
+    // 후크를 사용한 상태변수로 만들기
+    const [result,setResult] = React.useState(props.isChg);
+
+    // let result = props.isChg;
     // isChg속성은 true/false 데이터를 전달하여
     // MakeWork 컴포넌트의 변경여부를 결정함!
 
     // result 에 담긴 true/false값을 반대로 전환함!
     const again = () => {
-        result = !result;
+        // 후크 상태변수의 업데이트는 set변수를 사용한다!
+        setResult(!result);
+        // result = !result;
         console.log(result);
     }; ////////// again 함수 /////////
 
@@ -265,8 +270,12 @@ function ExpComp(props){ // isChg 는 true/false값 받는 속성
             <Title tit="명화" />
             {/* 2.변경버튼 : 클릭시 again함수를 호출함 */}
             <button onClick={again}>작가변경!!!</button>
-            {/* 3.작품출력 */}
-            <MakeWork painter="피카소" wname="우는여인" />
+            {/* 3.작품출력 : 3항연산자로 작품변경하기 */}
+            {
+                result ?
+                <MakeWork painter="피카소" wname="우는여인" /> :
+                <MakeWork painter="모네" wname="양산을 쓴 여인" />
+            }
         </React.Fragment>
     );
 
@@ -274,5 +283,46 @@ function ExpComp(props){ // isChg 는 true/false값 받는 속성
 
 
 // 컴포넌트 출력하기
-ReactDOM.render(<ExpComp isChg={true} />,
+ReactDOM.render(<ExpComp isChg={false} />,
 document.querySelector("#root5"));
+
+/*************************************************** 
+    [ 리액트 훅크 : React Hook ]
+    - 일반적으로 리액트에 사용되는 변수는 처음에 
+    컴포넌트에 전달되어 초기 셋팅에 활용된다.
+    그런데 이 변수가 변경될 경우 컴포넌트의 변경이
+    자동적으로 이루어지지 않는다!
+    이런 종류의 변수 업데이트가 가상돔과 실제돔에
+    바로 반영되도록 실시간 감시역할을 하는
+    리액트의 기술내용을 담고 있는 것이 후크다!
+
+    1. 목적 : 어떤 특정 데이터가 변경될때
+        이 데이터를 할당하여 사용하고 있는 컴포넌트의
+        변경이 반영되도록 하고자 할때 후크를 사용한다!
+
+    2. 구현방법:
+        1) 노드JS SPA 개발환경에서는 상단에 import useState를 한다!
+        -> CDN 에서는 React.useState 로 사용함!
+        2) 코딩법 : useState() 메서드사용
+            배열변수 = useState(초기값)
+            (CDN) -> 배열변수 = React.useState(초기값)
+
+            ((일반형))
+            const [변수명,set변수명] = useState(초기값)
+            -> set변수명 작성시 변수명 첫글자는 대문자로 씀
+            예) 변수명 myname -> setMyname
+            -> set변수명(값) : 메서드형태로 후크변수의 값을 셋팅함!
+
+        3) 작동원리 
+            - useState에 쓴 초기값이 배열변수 첫번째변수에 할당된다!
+            - 코드에서 set변수명에 값을 할당하면
+            useState메서드가 이것을 체크하여 이 변수를 사용한
+            다른부분의 업데이트를 실행한다!
+            예컨데 컴포넌트 내부에 사용한 경우 컴포넌트가 업데이트 됨!
+        4) 사용결과
+            - 별도의 메서드 호출없이 후크 상태변수를 사용한 곳이
+            자동으로 변경될대마다 다시 갱신되는 것을 확인 할 수 있다!
+
+        -> 뷰JS의 리액티브 데이터와 매우 유사함!
+
+***************************************************/
