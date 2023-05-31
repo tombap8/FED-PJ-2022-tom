c1881218809d7c10dabe8e0c8f182cc7e0d11210
Tom B. Parker <tombap8@hotmail.com>
2023-05-31 15:19:42 +0900

    ss

---
+++ 014.React학습/react-app/tom-app/src/Test.js
@@ -0,0 +1,11 @@
+
+function Test(){
+    return(
+      <>
+        <h1>나야나!</h1>
+        <img src='https://dimg.donga.com/wps/NEWS/IMAGE/2015/12/11/75316598.3.jpg' alt='모네그림' />
+      </>
+    );
+}
+
+export default Test;
