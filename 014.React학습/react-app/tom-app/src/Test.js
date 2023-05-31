88089663b83661d487e85ee39aec91c3309e8e1a
Tom B. Parker <tombap8@hotmail.com>
2023-05-31 16:00:46 +0900

    ss

--- 014.React학습/react-app/tom-app/src/Test.js
+++ 014.React학습/react-app/tom-app/src/Test.js
@@ -1,8 +1,9 @@
+import "./test.css";
 
 function Test(){
     return(
       <>
-        <h1>나야나!</h1>
+        <h1 className="tit">나야나!</h1>
         <img src='https://dimg.donga.com/wps/NEWS/IMAGE/2015/12/11/75316598.3.jpg' alt='모네그림' />
       </>
     );
