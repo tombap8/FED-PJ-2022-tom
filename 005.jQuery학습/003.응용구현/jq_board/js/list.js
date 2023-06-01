c402eb997a40e8d68b96aedab9a44548dea7c434
Tom B. Parker <tombap8@hotmail.com>
2023-06-01 10:05:10 +0900

    Update list.js

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -1,1 +1,20 @@
 // 리스트 페이지 JS - list.js
+
+let temp = "";
+
+for(let i = 0; i<50; i++){
+    temp += `
+        {
+            "idx" : "${i+1}",
+            "tit" : "게시판 제목입니다${i+1}",
+            "cont" : "게시판 내용입니다${i+1}",
+            "att" : "",
+            "date" : "2023-06-01",
+            "writer" : "관리자",
+            "pwd" : "1111",
+            "cnt" : "1"
+        },
+    `;
+}
+
+console.log(temp);
