0d3b75753817e6a104894fec0ab621f5ef16b7ba
Tom B. Parker <tombap8@hotmail.com>
2023-06-01 15:17:24 +0900

    Update Ban.js

--- 014.React학습/react-app/tom-app/src/dc/Ban.js
+++ 014.React학습/react-app/tom-app/src/dc/Ban.js
@@ -4,8 +4,20 @@ import "./css/ban.css";
 function Ban() {
 
     const ban_data = [
-        {},
-        {}
+        {
+            "src":"./images/dcm21.jpg",
+            "tit1":"GOTHAM GAZETTE",
+            "tit2":"WORLDS TRAVELER",
+            "cont":`Barry who? The Flash isn't the only DC hero this summer who's been traveling through the multiverse.`,
+            "btn":"New Places, Familiar Faces",
+        },
+        {
+            "src":"./images/dcm23.jpg",
+            "tit1":"GET TO KNOW",
+            "tit2":"CASSANDRA CAIN",
+            "cont":`Whether it's as Batgirl or Orphan, Cassandra Cain stands as one of the most compelling and beloved players in the Batman mythos.`,
+            "btn":"MEET CASS",
+        },
     ];
 
 
@@ -13,15 +25,12 @@ function Ban() {
         <div className="banner">
             <ul className="slider">
                 <li>
-                    <img className="banimg" src="./images/dcm21.jpg" alt="배너" />
+                    <img className="banimg" src={ban_data[1]["src"]} alt="배너" />
                     <section className="bantit">
-                        <h3>GOTHAM GAZETTE</h3>
-                        <h2>WORLDS TRAVELER</h2>
-                        <p>
-                            Barry who? The Flash isn't the only DC hero this summer who's been
-                            traveling through the multiverse.
-                        </p>
-                        <button>New Places, Familiar Faces</button>
+                        <h3>{ban_data[1]["tit1"]}</h3>
+                        <h2>{ban_data[1]["tit2"]}</h2>
+                        <p>{ban_data[1]["cont"]}</p>
+                        <button>{ban_data[1]["btn"]}</button>
                     </section>
                 </li>
             </ul>
