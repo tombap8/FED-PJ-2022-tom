9697dea05de7545ce3f59f8999e3c9de9e36f7b2
Tom B. Parker <tombap8@hotmail.com>
2023-06-01 15:04:48 +0900

    ss

--- 014.React학습/react-app/tom-app/src/dc/Ban.js
+++ 014.React학습/react-app/tom-app/src/dc/Ban.js
@@ -1,18 +1,31 @@
-
+// 배너 컴포넌트 - Ban.js
+import "./css/ban.css";
 
 function Ban() {
+
+    const ban_data = [
+        {},
+        {}
+    ];
+
+
     return (
-        <>
-            <img src="./images/dcm21.jpg" alt="배너" />
-            <section className="bantit">
-                <h3>GOTHAM GAZETTE</h3>
-                <h2>WORLDS TRAVELER</h2>
-                <p>
-                    Barry who? The Flash isn't the only DC hero this summer who's been traveling
-                    through the multiverse.
-                </p>
-            </section>
-        </>
+        <div className="banner">
+            <ul className="slider">
+                <li>
+                    <img className="banimg" src="./images/dcm21.jpg" alt="배너" />
+                    <section className="bantit">
+                        <h3>GOTHAM GAZETTE</h3>
+                        <h2>WORLDS TRAVELER</h2>
+                        <p>
+                            Barry who? The Flash isn't the only DC hero this summer who's been
+                            traveling through the multiverse.
+                        </p>
+                        <button>New Places, Familiar Faces</button>
+                    </section>
+                </li>
+            </ul>
+        </div>
     );
 } /////////// Ban 컴포넌트 /////////////
 
