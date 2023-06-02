788d8f551bcbdce6a25783545e4443749812c3d0
Tom B. Parker <tombap8@hotmail.com>
2023-06-02 12:24:29 +0900

    ss

--- 014.React학습/react-app/tom-app/src/index_first.js
+++ 014.React학습/react-app/tom-app/src/index_first.js
@@ -1,12 +1,12 @@
-// index.js는 public/index.html 페이지에 적용되는 컴포넌트다!
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-
+import "./index.css";
+import Ban from "./dc/Ban";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <>
-        
+        <Ban cat="main" />
+        <Ban cat="CHARACTERS" />
     </>
 );
