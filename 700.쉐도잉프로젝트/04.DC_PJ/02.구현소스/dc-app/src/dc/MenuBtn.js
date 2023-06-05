a1bdc95e82a30cea77090e34744aee8d24caecc0
Tom B. Parker <tombap8@hotmail.com>
2023-06-05 14:38:21 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/MenuBtn.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/MenuBtn.js
@@ -1,33 +1,33 @@
 ///  메뉴버튼 모듈 - MenuBtn.js
-import $ from 'jquery';
+import $ from "jquery";
 import "./css/menubtn.css";
 
 // 제이쿼리 로드구역 함수 /////////
-function jqFn(){
-    $(()=>{
-
-    }); //////// jQB ///////////
+function jqFn() {
+    $(() => {}); //////// jQB ///////////
 } ////////////// jQFn ///////////
 
-function MenuBtn(){
-    return(
+function MenuBtn() {
+    return (
         <>
-        <section className="menubtn">
-            <div className="imbx">
-                <img src="" alt="" />
-            </div>
-            <div className="titbx">
-                <h3></h3>
-                <h2></h2>
-            </div>
-            <div className="btnbx">
-                <button></button>
-            </div>  
-        </section>
-        {/* 빈루트를 만들고 JS로드함수포함 */}
-        {jqFn()}
+            <section className="menubtn">
+                <div>
+                    <div className="imbx">
+                        <img src="./images/menubtn1.jpg" alt="메뉴버튼" />
+                    </div>
+                    <div className="titbx">
+                        <h3>WORLDS COLLIDE</h3>
+                        <h2>The Flash is Only in Theaters June 16</h2>
+                    </div>
+                    <div className="btnbx">
+                        <button>TICKETS ON SALE NOW</button>
+                    </div>
+                </div>
+            </section>
+            {/* 빈루트를 만들고 JS로드함수포함 */}
+            {jqFn()}
         </>
-    )
+    );
 }
 
 export default MenuBtn;
