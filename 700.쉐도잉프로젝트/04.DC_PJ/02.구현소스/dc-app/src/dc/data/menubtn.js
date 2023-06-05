285858f713967234a91df5bde5cc5920b8f4a4ba
Tom B. Parker <tombap8@hotmail.com>
2023-06-05 15:16:22 +0900

    ss

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/menubtn.js
@@ -0,0 +1,28 @@
+// MenuBtn 컴포넌트용 데이터 - menubtn.js
+
+const menubtn_data = [
+    {
+        "isrc":"./images/menubtn1.jpg",
+        "tit":"WORLDS COLLIDE^The Flash is Only in Theaters June 16",
+        "btn":"TICKETS ON SALE NOW",
+        "link":"/co",
+    },
+    {
+        "isrc":"./images/menubtn2.jpg",
+        "tit":"DC SHOP^GET NUTS WITH THE 1/6 SCALE BATMAN FIGURE",
+        "btn":"Shop New Arrivals",
+        "link":"/mv",
+    },
+    {
+        "isrc":"./images/menubtn3.jpg",
+        "tit":"DC PRIDE^Celebrate with LGBTQ creators, characters, & stories",
+        "btn":"Visit our Pride Hub",
+        "link":"/gm",
+    },
+    {
+        "isrc":"./images/menubtn4.jpg",
+        "tit":"DC COMMUNITY ^Share Your DC Pride Fanworks",
+        "btn":"SUBMIT YOUR ART",
+        "link":"/nw",
+    },
+]
