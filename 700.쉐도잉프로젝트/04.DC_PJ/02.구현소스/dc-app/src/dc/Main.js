d0f0f64e70b49bc05c8bdfa9cf36ab22ac41f8bd
Tom B. Parker <tombap8@hotmail.com>
2023-06-05 12:58:59 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
@@ -1,10 +1,12 @@
 // DC 페인 페이지 컴포넌트
 import Ban from "./Ban";
+import MenuBtn from "./MenuBtn";
 
 const Main = () => {
     return(
         <>
             <Ban cat="main" />
+            <MenuBtn />
         </>
     );
 }; /////////// Main //////////////
