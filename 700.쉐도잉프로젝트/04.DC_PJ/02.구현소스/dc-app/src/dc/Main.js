96d6d2e0f3909ab488736811cc90ecbff241bd55
Tom B. Parker <tombap8@hotmail.com>
2023-06-07 11:11:28 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Main.js
@@ -1,12 +1,17 @@
 // DC 페인 페이지 컴포넌트
 import Ban from "./modules/Ban";
 import MenuBtn from "./modules/MenuBtn";
+import VidIntro from "./modules/VidIntro";
 
 const Main = () => {
-    return(
+    return (
         <>
+            {/* 1.배너모듈 */}
             <Ban cat="main" />
+            {/* 2. 메뉴버튼모듈 */}
             <MenuBtn />
+            {/* 3. 비디오소개모듈 */}
+            <VidIntro />
         </>
     );
 }; /////////// Main //////////////
--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidIntro.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/VidIntro.js
@@ -1,1 +1,37 @@
-// VidIntro 컴포넌트 JS
+// VidIntro 컴포넌트 JS - VidIntro.js
+
+import $ from 'jquery';
+import "../css/vidintro.css";
+
+// 제이쿼리 로드구역 함수 /////////
+function jqFn(){
+    $(()=>{
+
+    }); //////// jQB ///////////
+} ////////////// jQFn ///////////
+
+function VidIntro(){
+    return(
+        <>
+        {/* 모듈코드 */}
+        <section>
+            {/* 비디오파트 */}
+            <div>
+            <iframe src="{}" title="{}"></iframe>
+            </div>
+            {/* 타이틀파트 */}
+            <div>
+                <h3></h3>
+                <h2></h2>
+                <p></p>
+                <p></p>
+                {/* 링크있을경우 표시 */}
+            </div>
+        </section>
+        {/* 빈루트를 만들고 JS로드함수포함 */}
+        {jqFn()}
+        </>
+    )
+}
+
+export default VidIntro;
