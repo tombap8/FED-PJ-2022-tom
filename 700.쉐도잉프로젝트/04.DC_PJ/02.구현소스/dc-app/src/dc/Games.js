a23d873f1cdc6bcba4963e2eaa0f86e10f99928d
Tom B. Parker <tombap8@hotmail.com>
2023-06-07 15:17:10 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Games.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Games.js
@@ -1,11 +1,13 @@
 // DC Games 페이지 컴포넌트
 import React from "react";
 import Ban from "./modules/Ban";
+import VidIntro from "./modules/VidIntro";
 
 const Games = () => {
     return(
         <>
             <Ban cat="GAMES" />
+            <VidIntro pg="GAMES" mm="on" />
         </>
     );
 }; /////////// Games //////////////
