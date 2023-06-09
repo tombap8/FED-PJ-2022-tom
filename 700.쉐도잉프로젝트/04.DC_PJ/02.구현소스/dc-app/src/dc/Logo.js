bcaee9f2cbe667a5a6529cd9570a44766989f7b0
Tom B. Parker <tombap8@hotmail.com>
2023-06-09 11:16:36 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Logo.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Logo.js
@@ -6,16 +6,15 @@ const Logo = () => {
 
     //객체형 스타일적용: 속성명이 틀리면 아예 출력되지 않는다!
     const mystyle={
-        width:"81px",
-        height:"81px",
-        backgroundImage:"linear-gradient(45deg, #88f62f, #fa8104)",
+        width:"45px",
+        height:"45px",
         borderRadius:"50%"
         // outline:"3px solid lime"
     };
 
     return(
         <h1 style={mystyle}>
-            <img src={isrc.logo} style={{width:"81px"}} />
+            <img src={isrc.logo} style={{width:"45px"}} />
         </h1>
     );
 
