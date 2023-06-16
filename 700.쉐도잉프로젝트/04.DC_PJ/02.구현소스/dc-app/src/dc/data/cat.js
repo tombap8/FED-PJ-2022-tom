ea42ea92f8e2077a677091e423aa9814f76d62b1
Tom B. Parker <tombap8@hotmail.com>
2023-06-16 13:08:18 +0900

    SSS

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/cat.js
@@ -0,0 +1,214 @@
+// 캐릭터 컴포넌트 사용 데이터 - cat.js
+
+const cat_data = [
+    {
+        idx: "1",
+        tmsrc: "./images/cat/cat1.jpg",
+        cname: "SUPERMAN",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+    {
+        idx: "2",
+        tmsrc: "./images/cat/cat2.jpg",
+        cname: "BATMAN",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+    {
+        idx: "3",
+        tmsrc: "./images/cat/cat3.jpg",
+        cname: "WONDER WOMAN",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+    {
+        idx: "4",
+        tmsrc: "./images/cat/cat4.jpg",
+        cname: "GREEN LENTURN",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+    {
+        idx: "5",
+        tmsrc: "./images/cat/cat5.jpg",
+        cname: "THE FLASH",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+    {
+        idx: "6",
+        tmsrc: "./images/cat/cat6.jpg",
+        cname: "AQUAMAN",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+    {
+        idx: "7",
+        tmsrc: "./images/cat/cat7.jpg",
+        cname: "CYBORG",
+        cdesc: `Faster than a speeding bullet, more powerful than a locomotive… The Man of Steel fights a never-ending battle for truth, justice, and the American way.
+            ^
+            From his blue uniform to his flowing red cape to the "S" shield on his chest, Superman is one of the most immediately recognizable and beloved DC Super Heroes of all time. The Man of Steel is the ultimate symbol of truth, justice, and hope. He is the world's first Super Hero and a guiding light to all.
+        ^
+        The tip of the spear in a revolution that would change the landscape of pop culture, Superman has spent the last eighty years redefining what it means to stand for truth, justice and the American way. The last survivor of the doomed planet Krypton, raised in the quiet heartland of Smallville, Kansas, Superman is as much a legend as he is a man: the gold standard of heroism, compassion and responsibility.
+        ^
+        Though his powers make him god-like next to his human compatriots, Superman's story is not one of greed or conquest. Instead, he strives to represent the inherent goodness of the human spirit, and the capacity of every living thing to do right by their neighbors.`,
+        facts: `
+        Powers:
+        super strength, flight, invulnerability, super speed, heat vision, freeze breath, x-ray vision, superhuman hearing, healing factor
+        ^
+        First Appearance:
+        ACTION COMICS #1 (1938)
+        ^
+        Alias/Alter Ego:
+        Clark Kent, Kal-El
+        ^
+        Base of Operations:
+        Metropolis
+        ^
+        Occupation:
+        Reporter
+        `,
+        alignment: "hero",
+        type: "indiv",
+    },
+];
