64fe9854329010406fe72fd987af7a5deae810f2
Tom B. Parker <tombap8@hotmail.com>
2023-09-07 09:01:23 +0900

    ss

--- 004.JS학습/module/com.js
+++ 004.JS학습/module/com.js
@@ -1,6 +1,6 @@
 const domFn = {
     qs:x=>document.querySelector(x),
-    qsa:x=>{document.querySelectorAll(x)},
+    qsa:x=>document.querySelectorAll(x),
     addEvt:(ele, evt, fn)=>ele.addEventListener(evt, fn),
 }
 
