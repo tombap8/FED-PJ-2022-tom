3ec6e33ecb089a37091d3ad570012bcddcc856a3
Tom B. Parker <tombap8@hotmail.com>
2023-11-09 17:41:51 +0900

    ss

--- 014.React학습/react-ex/js/05.콜백.jsx
+++ 014.React학습/react-ex/js/05.콜백.jsx
@@ -1,20 +1,19 @@
 function MarkLike() {
-    const [masterOn, setMasterOn] = React.useState(false);
-    const [kitchenOn, setKitchenOn] = React.useState(false);
-    const [bathOn, setBathOn] = React.useState(false);
-  
-    const toggleMaster = () => {
-      setMasterOn(!masterOn);
-    };
-    const toggleKitchen = () => {
-      setKitchenOn(!kitchenOn);
-    };
-    const toggleBath = () => {
-      setBathOn(!bathOn);
-    };
+  const [masterOn, setMasterOn] = React.useState(false);
+  const [kitchenOn, setKitchenOn] = React.useState(false);
+  const [bathOn, setBathOn] = React.useState(false);
 
+  const toggleMaster = () => {
+    setMasterOn(!masterOn);
+  };
+  const toggleKitchen = () => {
+    setKitchenOn(!kitchenOn);
+  };
+  const toggleBath = () => {
+    setBathOn(!bathOn);
+  };
 
-    /* 
+  /* 
     useCallback은 첫 번째 인자로 넘어온 함수를, 
     두 번째 인자로 넘어온 배열 형태의 함수 실행 조건의 값이 
     변경될 때까지 저장해놓고 재사용할 수 있게 해 줍니다.
@@ -26,42 +25,44 @@ function MarkLike() {
     바뀌지 않는 한 기존 함수를 재사용할 수 있습니다.
     */
 
+  // const toggleMaster = React.useCallback(() => {
+  //   setMasterOn(!masterOn);
+  // }, [masterOn]);
+  // const toggleKitchen = React.useCallback(() => {
+  //   setKitchenOn(!kitchenOn);
+  // }, [kitchenOn]);
+  // const toggleBath = React.useCallback(() => {
+  //   setBathOn(!bathOn);
+  // }, [bathOn]);
 
-    // const toggleMaster = React.useCallback(() => {
-    //   setMasterOn(!masterOn);
-    // }, [masterOn]);
-    // const toggleKitchen = React.useCallback(() => {
-    //   setKitchenOn(!kitchenOn);
-    // }, [kitchenOn]);
-    // const toggleBath = React.useCallback(() => {
-    //   setBathOn(!bathOn);
-    // }, [bathOn]);
-  
-    return (
-      <div style={{textAlign:'center'}}>
-        <h1 style={{fontSize:'70px',color:'hotpink',textShadow:'5px 5px 2px #555'}}>좋아요~! 부탁해요!!</h1>
-        <ShowLike man="손흥민" on={kitchenOn} toggle={toggleKitchen}></ShowLike>
-        <ShowLike man="다나카" on={masterOn} toggle={toggleMaster}></ShowLike>
-        <ShowLike man="이강인" on={bathOn} toggle={toggleBath}></ShowLike>
-      </div>
-    );
-  }
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1
+        style={{
+          fontSize: "70px",
+          color: "hotpink",
+          textShadow: "5px 5px 2px #555",
+        }}
+      >
+        좋아요~! 부탁해요!!
+      </h1>
+      <ShowLike man="손흥민" on={kitchenOn} toggle={toggleKitchen}></ShowLike>
+      <ShowLike man="다나카" on={masterOn} toggle={toggleMaster}></ShowLike>
+      <ShowLike man="이강인" on={bathOn} toggle={toggleBath}></ShowLike>
+    </div>
+  );
+}
 
+const ShowLike = React.memo(({ man, on, toggle }) => {
+  console.log({ man, on });
+  return (
+    <div style={{ padding: "10px" }}>
+      <button onClick={toggle} style={{ fontSize: "100px" }}>
+        {man}
+        {on ? "👍" : "👎"}
+      </button>
+    </div>
+  );
+});
 
-  const ShowLike = React.memo(
-    ({ man, on, toggle })=> {
-    console.log({ man, on });
-    return (
-      <div style={{padding:'10px'}}>
-        <button onClick={toggle}
-        style={{fontSize:"100px"}}>
-          {man}
-          {on ? "👍" : "👎"}
-        </button>
-      </div>
-    );
-  })
-
-
-  
-ReactDOM.render(<MarkLike />,document.querySelector('#root'))
+ReactDOM.render(<MarkLike />, document.querySelector("#root"));
