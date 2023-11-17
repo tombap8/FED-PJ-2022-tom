06f577036e280080967897ac407c6c281df44dad
Tom B. Parker <tombap8@hotmail.com>
2023-11-17 16:52:11 +0900

    ss

--- 014.React학습/react-app/tom-app/src/dc/plugin/SwiperApp.jsx
+++ 014.React학습/react-app/tom-app/src/dc/plugin/SwiperApp.jsx
@@ -1,6 +1,5 @@
 // 스와이퍼 플러그인 컴포넌트
 
-import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -15,9 +14,9 @@ import "./css/swiper.css";
 // 사용할 스와이퍼 모듈을 불러온다
 // (여기서는 페이지네이션,네비게이션,자동넘김)
 import { Pagination,Navigation,Autoplay } from "swiper/modules"
+import { useRef, useState } from "react";
 
 export function SwiperApp() {
-  const myRef = useRef(null);
   // 불러올 이미지 리스트
   const imgArr = [
     "dcm28",
@@ -30,18 +29,43 @@ export function SwiperApp() {
     "dcm12",
   ];
 
-  let [sts,setSts] = useState(1);
+  let myFirst = '스와이퍼';
+  const mySecond = useRef('갤러리');
+
+  const myFn = () => {
+    myFirst = 'Swiper';
+    mySecond.current = 'Gallery';
+    console.log('함수호출:',myFirst,mySecond.current);
+  }
+
+  // 상태관리변수 : 멈춤상태 / 플레이상태
+  const [sts,setSts] = useState(1);
+
+  // useRef로 스와이퍼 객체를 받아온다!
+  const myRef = useRef(null);
+  // Swiper 컴포넌트의 ref속성에 담아서 연결!!!
+
+  // 플레이/멈춤기능 함수
   const stopPlay = () => {
-    console.log('ㅎㅎ');
+    console.log('멈추거나 플레이!');
+    // sts값이 1이면 멈춤!
     sts?
     myRef.current.swiper.autoplay.stop():
     myRef.current.swiper.autoplay.start();
-    sts?setSts(0):setSts(1);
-  }
 
+    // 상태값 업데이트
+    sts? setSts(0) : setSts(1);
+    
+    console.log('리랜더링:',myFirst,mySecond.current);
+  }; ///////// stopPlay함수 /////////
+
+
+  // 리턴코드 ///////////////////
   return (
     <>
+      <h1 className="swTit">{myFirst+' : '+mySecond.current}</h1>
       <Swiper
+      /* ref 속성에 useRef 할당변수를 넣어서 외부에 연결함 */
         ref={myRef}
         slidesPerView={3}
         spaceBetween={30}
@@ -65,9 +89,22 @@ export function SwiperApp() {
             </SwiperSlide>)
         }        
       </Swiper>
-       <button 
-       style={{position:'relative',top:'20px',zIndex:10}} 
-       onClick={stopPlay}>{sts?'멈춤':'재생'}</button>
+      {/* 플레이/멈춤버튼 */}      
+      <button 
+      className="stopPlay"
+      style={{
+        backgroundColor: 'transparent',
+        border: 'none',
+        fontSize: '40px',
+        display: 'block',
+        width: '40px',
+        margin: '0 auto',
+        cursor: 'pointer'
+      }}
+      title={sts?"멈추기":"자동넘기기"}
+      onClick={stopPlay}
+      >{sts?'▣':'▶'}</button>
+      <button onClick={myFn}>useRef 테스트</button>
     </>
   );
 } /////////// SwiperApp 컴포넌트 ///////////
