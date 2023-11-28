d5369408ddd372783d55186ea6ce3f874f3772d8
Tom B. Parker <tombap8@hotmail.com>
2023-11-28 15:02:42 +0900

    Update Searching.jsx

--- 702.쉐도잉프로젝트현재/dc-app/src/components/modules/Searching.jsx
+++ 702.쉐도잉프로젝트현재/dc-app/src/components/modules/Searching.jsx
@@ -5,25 +5,79 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SchCatList } from "./SchCatList";
 
+// 제이쿼리
+import $ from "jquery";
+
 // 검색모듈용 CSS 불러오기
 import "../../css/searching.css";
-import { memo } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 
-export const Searching = memo(({word}) => {
+export function Searching(props) {
   // props.kword - 검색어전달
-  console.log("전달검색어:", word);
+  console.log("전달검색어:", props.kword);
+
+  ////////// 후크 상태관리 변수 ////////////
+  // 1. 검색어 후크상태변수 : 초기값은 전달된 검색어
+  const [kword,setKword] = useState(props.kword)
+  // 2. 출력개수 후크상태변수
+  const [cntNum,setCntNum] = useState(0);
+  //////////////////////////////////////////
+
+  let mm = useRef(1);
+
+  // 검색어 업데이트 함수 /////
+  const chgKword = txt => setKword(txt);
+
+  const initFn = () => {
+    // 넘어온 검색어와 셋팅된 검색어가 다르면 업데이트
+    if(props.kword!=kword){ 
+      chgKword(props.kword);
+      // 모듈검색 input창에 같은 값 넣어주기
+      $('#schin').val(props.kword);
+    } ///////// if ///////////
+
+  }
+
+  if(mm.current) initFn();
+
+
+  // 리스트 개수변경함수 ///////
+  const chgCnt = (num) => {
+    // 후크 상태개수변수 업데이트하기
+    setCntNum(num);
+    // $('.cntNum').text(num);
+  }; ///////// showCnt 함수 ///////
 
   // 검색리스트 만들기 함수
-  const schList = () => {};
+  const schList = (e) => {
+    // console.log(e.currentTarget);
+    // 아이콘 다음요소가 input 이고 그 값을 읽어와서 변경
+    chgKword($(e.currentTarget).next().val())
+  };
 
   // 엔터키 반응 함수
-  const enterKey = () => {};
+  const enterKey = (e) => {
+    mm.current = 0;
+    // 엔터키일때만 반영함
+    if(e.key == 'Enter'){
+      let txt = $(e.target).val();
+      chgKword(txt)
+      console.log(txt,e.key);
+    }
+    setTimeout(() => {
+      
+      mm.current = 1;
+    }, 500);
+  };
 
   // 체크박스검색 함수 ////////
   const chkSearch = () => {};
 
   // 리스트 정렬 함수 /////////
   const sortList = () => {};
+
 
   // 리턴 코드 ////////////////////////
   return (
@@ -47,7 +101,14 @@ export const Searching = memo(({word}) => {
               type="text"
               placeholder="Filter by Keyword"
               onKeyUp={enterKey}
-              defaultValue={word}
+              defaultValue={kword}
+              /* input요소에서 리액트 value속성은 
+              defaultValue를 사용한다! -> 처음입력값 
+              _________________________________
+              value속성을 쓰면 동적변경이 이루어지고
+              사용자가 입력하지 못하도록 readOnly(읽기전용)
+              설정이 되어 있어야한다! */
+              
             />
           </div>
           {/* 1-2. 체크박스구역 */}
@@ -112,7 +173,7 @@ export const Searching = memo(({word}) => {
         <div className="listbx">
             {/* 2-1. 결과 타이틀 */}
             <h2 className="restit">
-                BROWSE CHARACTERS (total)
+                BROWSE CHARACTERS ({cntNum})
             </h2>
             {/* 2-2. 정렬선택박스 */}
             <aside className="sortbx">
@@ -126,10 +187,14 @@ export const Searching = memo(({word}) => {
                     <option value="1">Z-A</option>
                 </select>
             </aside>
-            {/* 2-3. 캐릭터 리스트 컴포넌트 */}
-            <SchCatList kword={word} />
+            {/* 2-3. 캐릭터 리스트 컴포넌트 : 
+            검색어를 후크상태변수로 연결! -> 데이터변경에 반영 */}
+            <SchCatList 
+              word={kword} 
+              chgCntFn={chgCnt}
+            />
         </div>
       </section>
     </>
   );
-}); ////////////// Searching 컴포넌트 //////////
+} ////////////// Searching 컴포넌트 //////////
